93dd2939c37263384d4309e9c7cd111fdfc1c282
ESosa0 <ericasosa0@gmail.com>
2016-04-11 16:49:05 +0100

    added amazon button and link to product page

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -6,7 +6,9 @@ $(function() {
 
   $('.more-info').click(function(event){
     event.preventDefault();
+
     $('#book-info').empty();
+
     var name = $(event.currentTarget).data('name');
     var author = $(event.currentTarget).data('author');
     var isbn = $(event.currentTarget).data('primaryIsbn13');
@@ -16,13 +18,6 @@ $(function() {
     var imageUrl = $(event.currentTarget).data('image')
     var rank = $(event.currentTarget).data('rank')
 
-    console.log('name = ' + name);
-    console.log('author = ' + author);
-    console.log('isbn = ' + isbn);
-    console.log('description = ' + description);
-    console.log('weeks on list = ' + weeksOnList);
-    console.log('amazon link = ' + amazonProductUrl);
-
     $('#book-info').append('<img src= ' + imageUrl + '>' );
     $('#book-info').append('<h3>' + name + '<h3>');
     $('#book-info').append('<h4>' + author + '<h4>');
@@ -30,7 +25,22 @@ $(function() {
     $('#book-info').append('<p> Rank: ' + rank + '</p>');
     $('#book-info').append('<p> Number of weeks on Best Seller list: ' + weeksOnList + '</p>');
     $('#book-info').append('<p> Description: ' + description + '</p>');
-    
+
+    $('#buy-button').removeClass('hidden');
+    $('#book-info').removeClass('hidden');
+
+    $('#buy-button').click(function(event){
+      event.preventDefault();  
+      window.location.href = amazonProductUrl;
+    }); 
   });
+
+
+
+
+
+
+
+
 
 });
